18503898fdc55ed39e3476dc606f1c4f5757e896
S-P-H-E <sphe.g.personal@gmail.com>
2025-05-29 21:40:02 +0200

    snap

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,9 @@ export const metadata: Metadata = {
     description: "Crafting digital experiences that leave lasting impressions.",
     images: [
       {
-        url: '/screenshot.png?v=1',
-        width: 1200,
-        height: 630,
+        url: '/screenshot-v1.png',
+        width: 1600,
+        height: 1200,
         alt: 'Screenshot of 82 Studio website',
       },
     ],
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: "Eighty Two Studio",
     description: "Crafting digital experiences that leave lasting impressions.",
-    images: ['/screenshot.png?v=1'],
+    images: ['/screenshot-v1.png'],
   },
 };
 
